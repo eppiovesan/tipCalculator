4c104a8e71abc7b9fbe9a7422eb3d431cd9a3097
Elize Piovesan <elizepiovesan.sci@gmail.com>
2025-04-20 20:51:57 -0300

    Exibição dos valores na tela de resultados.

--- app/src/main/java/com/example/tipCalculator/resultActivity.kt
+++ app/src/main/java/com/example/tipCalculator/resultActivity.kt
@@ -3,20 +3,43 @@ package com.example.tipCalculator
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
 
 class resultActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
+        val viewModel = (application as tipCalculator).sharedViewModel
 
-        val btn_recalcular = findViewById<Button>(R.id.btn_reacalcular)
 
-        btn_recalcular.setOnClickListener {
+        val valorConta = viewModel.valorConta
+        val numeroPessoas = viewModel.numeroPessoas
+        val percentualGorjeta = (viewModel.percentualGorjeta) * 100
+        val valorGorjeta = String.format("%.2f", viewModel.calculaGorjeta()).toFloat()
+        val valorPessoa = String.format("%.2f", viewModel.calculaValorFinalConta()).toFloat()
+
+        val tvValorConta = findViewById<TextView>(R.id.txt_valor_conta)
+        val tvNumeroPessoas = findViewById<TextView>(R.id.txt_qtde_pessoas)
+        val tvPercentualGorjeta = findViewById<TextView>(R.id.txt_percentual_gorjeta)
+        val tvValorGorjeta = findViewById<TextView>(R.id.txt_valor_gorjeta)
+        val tvValorPorPessoa = findViewById<TextView>(R.id.text_result)
+
+        val btnRecalcular = findViewById<Button>(R.id.btn_reacalcular)
+
+        tvValorConta.text = valorConta.toString()
+        tvNumeroPessoas.text = numeroPessoas.toString()
+        tvPercentualGorjeta.text = percentualGorjeta.toString()
+        tvValorGorjeta.text = valorGorjeta.toString()
+        tvValorPorPessoa.text = valorPessoa.toString()
+
+        btnRecalcular.setOnClickListener {
             startActivity(Intent(this,tipInfoActivity::class.java))
+            finish()
         }
 }
 
